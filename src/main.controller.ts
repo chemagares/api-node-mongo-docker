6900dd505127812513559a91fe7da5d6322dd6c3
José María García Espada <jigc@gft.com>
2020-04-08 14:55:08 +0200

    initial

---
+++ src/main.controller.ts
@@ -0,0 +1,17 @@
+//src/main.controller.ts
+
+import { Application } from 'express';
+import { PokeService } from './services/pokemon.service';
+
+export class Controller {
+  private pokeService: PokeService;
+
+  constructor(private app: Application) {
+    this.pokeService = new PokeService();
+    this.routes();
+  }
+
+  public routes() {
+    this.app.route('/').get(this.pokeService.welcomeMessage);
+  }
+}
---
+++ src/services/pokemon.service.ts
@@ -0,0 +1,9 @@
+//src/services/pokemon.service.ts
+
+import { Request, Response } from 'express';
+
+export class PokeService {
+  public welcomeMessage(req: Request, res: Response) {
+    return res.status(200).send('Welcome to pokeAPI REST by Nya ^^');
+  }
+}
